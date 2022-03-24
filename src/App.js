1be5d7cdc97001e040a2c63cdd2ae0ec7c2f28f6
RjHuffaker <ryanjayhuffaker@gmail.com>
2022-03-24 11:28:52 -0600

    Added H2 tag

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>Voipinator</h1>
+      <h2>Voiping voipishly across the Voipasphere</h2>
       <p>Some stuff about Voipinator</p>
     </div>
   );
