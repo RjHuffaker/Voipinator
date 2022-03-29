bb61aacfaa04974e4e8f962deaba0985c02dbb86
RjHuffaker <ryanjayhuffaker@gmail.com>
2022-03-29 16:05:18 -0600

    Changed formatting

--- src/pages/annual.js
+++ src/pages/annual.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
 const AnnualReport = () => {
-return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>Annual Report</h1>
-	</div>
-);
+	return (
+		<div style={{
+
+		}}>
+			<h1>Annual Report</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	);
 };
 
 export default AnnualReport;
